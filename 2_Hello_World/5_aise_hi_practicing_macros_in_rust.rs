16706d6cda4932f693945c9ed9a1330cf8628d8e
Parth Salunke <144267660+sparth292@users.noreply.github.com>
2025-08-20 23:19:06 +0530

    Create 5_aise_hi_practicing_macros_in_rust.rs

---
+++ 2_Hello_World/5_aise_hi_practicing_macros_in_rust.rs
@@ -0,0 +1,19 @@
+// ye code me baadme samjhaunga jab humlog rust macros deeply seekhenge tab
+
+macro_rules! say_hola
+{
+
+() => {
+  
+  println!("Hola Amigo , Kaise ho theek ho");
+  println!("Dies out of cringe after printing that line");
+
+};
+
+}
+
+fn main(){
+
+   say_hola!();
+
+}
