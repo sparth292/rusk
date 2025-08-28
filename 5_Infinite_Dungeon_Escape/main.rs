a27a2c6a9a8e8c18d3bab754ea646c0473b761fc
Parth Salunke <144267660+sparth292@users.noreply.github.com>
2025-08-28 20:26:48 +0530

    Update main.rs

--- 5_Infinite_Dungeon_Escape/main.rs
+++ 5_Infinite_Dungeon_Escape/main.rs
@@ -1,3 +1,4 @@
+use std::hash::BuildHasherDefault;
 use std::io; 
 use std::io::Write;
 use rand::Rng;
@@ -7,38 +8,46 @@ fn main(){
    let mut health = 100;
    let mut choice = String::new();
    loop{
-     
+     if health <= 0 {
+            println!("GAME OVER! You died with {} HP", health);
+            break;
+     }
      print!("Enter Your Choice : \n\n");
      io::stdin().read_line(&mut choice).expect("Failed to take input\n\n");
   
-     if choice == "W" {
+     if choice.trim() == "W" {
         print!("W , you lost your 30 hp\n\n");
+        health -= 30;
         choice.clear();
      }
-     else if choice == "A" {
+     else if choice.trim() == "A" {
         print!("IDK why but bcoz of my mood swings you lost 50 hp \n\n");
         health -= 50;
         choice.clear();
      }
-     else if choice == "S"
+     else if choice.trim() == "S"
      {
         print!("You lost , Life is unfair \n\n");
-        health -= 100;
         choice.clear();
+        break;
      }
-     else if choice == "D"{
+     else if choice.trim() == "D"{
         print!("I dont know why , but you won\n\n");
         choice.clear();
         health = 200;
      }
-     else if choice == "Q"{
+     else if choice.trim() == "Q"{
         print!("You are stuck in the Dungeon 4 Ever\n\n");
         choice.clear();
-        std::process::exit(0);
+        break;
+     }
+     else{
+        print!("Invalid Choice \n\n");
+        choice.clear();
      }
    
 
-     
+     // Aage proceed hoga
 
 
    }
