9a3de9adaa1e5e47d92eccf438bdd490da1c46e3
Parth Salunke <144267660+sparth292@users.noreply.github.com>
2025-09-28 09:36:34 +0530

    Create 1_Variable_Example.rs

---
+++ 6_Variable_And_Mutability/1_Variable_Example.rs
@@ -0,0 +1,19 @@
+// This might give you a warning like this
+// warning: variable does not need to be mutable
+//  --> /tmp/23CMfFF3Ry/main.rs:5:10
+//   |
+// 5 |      let mut x = 19;
+//   |          ----^
+//   |          |
+//   |          help: remove this `mut`
+//   |
+//   = note: `#[warn(unused_mut)]` on by default
+
+// warning: 1 warning emitted
+
+fn main() {
+     let mut x = 19;
+     println!("{x}");
+     x = 10;
+     println!("{x}");
+}
