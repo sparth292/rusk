8f9b2631df566df35aa2c447427ae79f9db9c6d2
Parth Salunke <144267660+sparth292@users.noreply.github.com>
2025-10-02 17:27:57 +0530

    Create 1_For_In_Loop_Rust.rs

---
+++ 7_Loops_In_Rust/1_For_In_Loop_Rust.rs
@@ -0,0 +1,12 @@
+// Online Rust compiler to run Rust program online
+// Print "Try programiz.pro" message
+
+fn main() {
+    println!("Loops in Rust");
+    let number = 15;
+    
+    for _i in 1..12{
+        let result = _i * number;
+        println!("{_i} X {number} = {result}");
+    }
+}
